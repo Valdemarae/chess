a968f2c11ada6734db15c0c5073bacf5c28d26fd
Valdemar <valdemar.kobelis@gmail.com>
2023-08-28 17:18:45 +0300

    Add ability to replace existing data instead of creating additional saved game

--- lib/game.rb
+++ lib/game.rb
@@ -226,8 +226,25 @@ class Game
 
   def save_game
     name = "#{@first_player.name} vs #{@second_player.name}"
-    data = YAML.dump({name: name, board: @board, player1: @first_player, player2: @second_player})
-    File.open('saved_games/saved_data.yml', 'a') {|f| f.write data}
+    data_array = YAML.load_stream(File.read('saved_games/saved_data.yml'))
+    changed = false
+    data_array.each_with_index do |saved_data, index|
+      if saved_data[:name] == name
+        data_array[index][:board] = @board
+        data = YAML.dump(data_array[0])
+        File.open('saved_games/saved_data.yml', 'w') {|f| f.write data}
+        changed = true
+      end
+    end
+    if changed
+      for i in 1...data_array.length do
+        data = YAML.dump(data_array[i])
+        File.open('saved_games/saved_data.yml', 'a') {|f| f.write data}
+      end
+    else
+      data = YAML.dump({name: name, board: @board, player1: @first_player, player2: @second_player})
+      File.open('saved_games/saved_data.yml', 'a') {|f| f.write data}
+    end
     puts "\e[32mThe game has been saved and named as '#{name}'.\e[0m"
     puts "\e[32mNow you can type 'exit' and continue the game next time.\e[0m"
   end
