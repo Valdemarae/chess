806dba70b1039d75ba1c7fdd2aa5420b35d84e77
Valdemar <valdemar.kobelis@gmail.com>
2023-08-28 16:29:41 +0300

    Add feature to load saved game

--- lib/game.rb
+++ lib/game.rb
@@ -6,8 +6,12 @@ require_relative 'player'
 class Game
   def play
     print_introduction
-    create_players
-    @board = Board.new
+    if new_or_existing_game == 'existing'
+      load_game
+    else
+      create_players
+      @board = Board.new
+    end
     @hash = @board.board_hash
     @moves = PossibleMoves.new
     @board.print_board
@@ -221,9 +225,44 @@ class Game
 
   def save_game
     name = "#{@first_player.name} vs #{@second_player.name}"
-    data = YAML.dump ({name: name, board: @board, player1: @first_player, player2: @second_player})
+    data = YAML.dump({name: name, board: @board, player1: @first_player, player2: @second_player})
     File.open('saved_games/saved_data.yml', 'a') {|f| f.write data}
     puts "\e[32mThe game has been saved and named as '#{name}'.\e[0m"
     puts "\e[32mNow you can type 'exit' and continue the game next time.\e[0m"
+  end
+
+  def load_game
+    data_array = YAML.load_stream(File.read('saved_games/saved_data.yml'))
+    data = get_game_data(data_array)
+    @board = data[:board]
+    @first_player = data[:player1]
+    @second_player = data[:player2]
+  end
+
+  def get_game_data(data_array)
+    data_array.each_with_index do |data, index|
+      puts "[#{index + 1}] #{data[:name]}"
+    end
+    index = nil
+    loop do
+      print 'Choose your saved game number: '
+      index = gets.chomp.to_i - 1
+      break if index.between?(0, data_array.length - 1)
+    end
+    print_line
+    data_array[index]
+  end
+
+  def new_or_existing_game
+    puts '[1] New game'
+    puts '[2] Load saved game'
+    result = nil
+    loop do
+      print 'Choose number: '
+      result = gets.chomp.to_i
+      break if result.between?(1,2)
+    end
+    print_line
+    'existing' if result == 2
   end
 end
